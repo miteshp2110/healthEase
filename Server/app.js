6591cf04bb4fa552db53c9d30141e45d4a964635
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-03-22 23:18:48 +0530

    Added /

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,12 @@ require('dotenv').config()
 
 
 showStatus()
+app.get('/',((req,res)=>{
+    res.json({"/hospital?city=<cityname>&speciality=<specialization>":"For getting results of hospital on basis of city and search. You can also use any either of it",
+    "/search/city?name=<cityname>":"This endpoint is to give search results of all cities in search bar",
+    "/search/speciality/?specialization=<specialization>":"This endpoint is for getting search result of specialization."
+})
+}))
 app.use('/',require('./routes/hospitalRoute'))
 app.use('/search',require('./routes/searchRoute'))
 
